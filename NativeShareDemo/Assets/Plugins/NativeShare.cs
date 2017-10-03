56f331c016d4696fa7083d85591e3118722a6c50
Süleyman Yasir KULA <yasirkula@yahoo.com>
2017-10-03 23:58:08 +0300

    Added subject+message

--- NativeShareDemo/Assets/Plugins/NativeShare.cs
+++ NativeShareDemo/Assets/Plugins/NativeShare.cs
@@ -10,57 +10,48 @@ using System.IO;
 public static class NativeShare
 {
 #if !UNITY_EDITOR && UNITY_IOS
-    [System.Runtime.InteropServices.DllImport( "__Internal" )]
-    private static extern void _ShareImage( string path );
+	[DllImport( "__Internal" )]
+	private static extern void _ShareText( string subject, string message );
 
-    [System.Runtime.InteropServices.DllImport( "__Internal" )]
-    private static extern void _ShareVideo( string path );
+	[DllImport( "__Internal" )]
+	private static extern void _ShareImage( string path, string subject, string message );
+
+	[DllImport( "__Internal" )]
+	private static extern void _ShareVideo( string path, string subject, string message );
 #endif
 
-    public static void Share( string mediaPath, bool isPicture, string authority = null )
-    {
-		if( !File.Exists( mediaPath ) )
-			throw new FileNotFoundException( "File not found at " + mediaPath );
+	public static void Share( string mediaPath, string subject, string message, bool isMediaImage, string authority = null )
+	{
+		if( mediaPath == null )
+			mediaPath = string.Empty;
 
+		if( subject == null )
+			subject = string.Empty;
+
+		if( message == null )
+			message = string.Empty;
+
+		if( mediaPath.Length > 0 && !File.Exists( mediaPath ) )
+			throw new FileNotFoundException( "File not found at " + mediaPath );
+		
 #if UNITY_EDITOR
 #elif UNITY_ANDROID
 		if( authority == null || authority.Length == 0 )
 			throw new System.ArgumentException( "Parameter 'authority' is null or empty!" );
 
-        using( AndroidJavaClass unityClass = new AndroidJavaClass( "com.unity3d.player.UnityPlayer" ) )
+		using( AndroidJavaClass unityClass = new AndroidJavaClass( "com.unity3d.player.UnityPlayer" ) )
 		using( AndroidJavaObject context = unityClass.GetStatic<AndroidJavaObject>( "currentActivity" ) )
 		using( AndroidJavaClass nativeShare = new AndroidJavaClass( "com.yasirkula.unity.NativeShare" ) )
-        {
-            nativeShare.CallStatic( "MediaShareFile", context, mediaPath, authority, isPicture );
-        }
-		
-		// This method (not using a ContentProvider) does not work for LinkedIn and Google+ for some devices (for some reason?)
-		// thus, it is replaced with a method that uses ContentProvider
-		/*AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-		AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
-
-		intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
-		AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
-		AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse", "file://" + mediaPath);
-		intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
-
-        if( isPicture )
-		    intentObject.Call<AndroidJavaObject>("setType", "image/png");
-        else
-            intentObject.Call<AndroidJavaObject>("setType", "video/mp4");
-
-		intentObject.Call<AndroidJavaObject>("setFlags", intentClass.GetStatic<int>("FLAG_GRANT_READ_URI_PERMISSION"));
-
-		AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
-
-		AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "");
-		currentActivity.Call("startActivity", jChooser);*/
+		{
+			nativeShare.CallStatic( "Share", context, mediaPath, subject, message, authority, isMediaImage );
+		}
 #elif UNITY_IOS
-        if( isPicture )
-		    _ShareImage( mediaPath );
-        else
-            _ShareVideo( mediaPath );
+		if( mediaPath.Length == 0 )
+			_ShareText( subject, message );
+		else if( isMediaImage )
+		    _ShareImage( mediaPath, subject, message );
+		else
+			_ShareVideo( mediaPath, subject, message );
 #else
 		Debug.Log( "No sharing set up for this platform." );
 #endif
--- NativeShareDemo/Assets/Plugins/iOS/NativeShare.mm
+++ NativeShareDemo/Assets/Plugins/iOS/NativeShare.mm
@@ -8,12 +8,14 @@ extern UIViewController* UnityGetGLViewController();
 
 #endif
 
-extern "C" void _ShareImage(const char* path) 
+extern "C" void _ShareText(const char* subject, const char* message) 
 {
-	UIImage *image = [UIImage imageWithContentsOfFile:[NSString stringWithUTF8String:path]];
-	NSArray *items = @[image];
+	NSArray *items = @[[NSString stringWithUTF8String:message]];
 	
 	UIActivityViewController *activity = [[UIActivityViewController alloc] initWithActivityItems:items applicationActivities:Nil];
+	if (strlen(subject) > 0)
+		[activity setValue:[NSString stringWithUTF8String:subject] forKey:@"subject"];
+	
 	UIViewController *rootViewController = UnityGetGLViewController();
     //if iPhone
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
@@ -27,13 +29,46 @@ extern "C" void _ShareImage(const char* path)
     }
 }
 
-extern "C" void _ShareVideo(const char* path) 
+extern "C" void _ShareImage(const char* path, const char* subject, const char* message) 
+{
+	UIImage *image = [UIImage imageWithContentsOfFile:[NSString stringWithUTF8String:path]];
+	NSArray *items;
+	if (strlen(message) > 0)
+		items = @[image];
+	else
+		items = @[[NSString stringWithUTF8String:message], image];
+	
+	UIActivityViewController *activity = [[UIActivityViewController alloc] initWithActivityItems:items applicationActivities:Nil];
+	if (strlen(subject) > 0)
+		[activity setValue:[NSString stringWithUTF8String:subject] forKey:@"subject"];
+	
+	UIViewController *rootViewController = UnityGetGLViewController();
+    //if iPhone
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
+          [rootViewController presentViewController:activity animated:YES completion:Nil];
+    }
+    //if iPad
+    else {
+        // Change Rect to position Popover
+        UIPopoverController *popup = [[UIPopoverController alloc] initWithContentViewController:activity];
+        [popup presentPopoverFromRect:CGRectMake(rootViewController.view.frame.size.width/2, rootViewController.view.frame.size.height/4, 0, 0)inView:rootViewController.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    }
+}
+
+extern "C" void _ShareVideo(const char* path, const char* subject, const char* message) 
 {
 	NSString *videoPath = [NSString stringWithUTF8String:path];
 	NSURL *videoURL = [NSURL fileURLWithPath:videoPath];
-	NSArray *items = @[videoURL];
+	NSArray *items;
+	if (strlen(message) > 0)
+		items = @[videoURL];
+	else
+		items = @[[NSString stringWithUTF8String:message], videoURL];
 	
 	UIActivityViewController *activity = [[UIActivityViewController alloc] initWithActivityItems:items applicationActivities:Nil];
+	if (strlen(subject) > 0)
+		[activity setValue:[NSString stringWithUTF8String:subject] forKey:@"subject"];
+	
 	UIViewController *rootViewController = UnityGetGLViewController();
     //if iPhone
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone) {
