2bf03665d97aec4baf85bef316f24b5e145f0789
Süleyman Yasir KULA <yasirkula@yahoo.com>
2020-09-12 18:27:43 +0300

    Replaced default value of PHOTO_LIBRARY_USAGE_DESCRIPTION with a better description on iOS

--- Plugins/NativeShare/Editor/NSPostProcessBuild.cs
+++ Plugins/NativeShare/Editor/NSPostProcessBuild.cs
@@ -9,7 +9,7 @@ using UnityEditor.iOS.Xcode;
 public class NSPostProcessBuild 
 {
 	private const bool ENABLED = true;
-	private const string PHOTO_LIBRARY_USAGE_DESCRIPTION = "Save media to Photos";
+	private const string PHOTO_LIBRARY_USAGE_DESCRIPTION = "The app requires access to Photos to save media to it.";
 
 	[InitializeOnLoadMethod]
 	public static void ValidatePlugin()
